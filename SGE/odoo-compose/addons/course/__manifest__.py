7a4e97275764dc537ce1c61017f33ac5d278e496
DanielRemiro <remirolaguna23@iespabloserrano.com>
2025-11-13 14:12:58 +0100

    upd

---
+++ SGE/odoo-compose/addons/course/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "course",
+    'summary': """examen odoo""",
+    'description': "my first module in odoo 19 -",
+    'author': "Daniel",
+    'website': "http://www.iespabloserrano.com",
+
+    'category': 'odoo',
+    'version': '0.1',
+
+    'data': [
+        'views.xml',
+    ],
+
+    'demo':[
+        'demo/demo.xml'
+    ]
+}
--- SGE/odoo-compose/addons/course/models.py
+++ SGE/odoo-compose/addons/course/models.py
@@ -3,8 +3,8 @@
 from odoo import models, fields, api
 
 class Course(models.Model):
-    _name = 'courses.course'
-    _description = 'Course'
+    _name = 'course.course'
+    _description = 'Me encanta ODOO'
 
     name = fields.Char(string='Course Name', required=True)
     description = fields.Text(string='Description')
