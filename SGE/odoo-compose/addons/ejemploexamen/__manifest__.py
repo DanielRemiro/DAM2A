82359a5e4fe77ed9489636631af4f6d04c8e97c6
Daniel Remiro <remirolaguna23@iespabloserrano.com>
2025-11-27 19:03:41 +0100

    upd

---
+++ SGE/odoo-compose/addons/ejemploexamen/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "examen_odoo",
+    'summary': "me encanta odoo 19",
+    'description': "Examen Odoo primera evaluacion",
+    'author': "Daniel Remiro",
+    'website': "http://www.iespabloserrano.com",
+
+    'category': 'odoo',
+    'version': '0.1',
+
+    'data': [
+        'views.xml',
+
+    ],
+}
---
+++ SGE/odoo-compose/addons/ejemploexamen/models.py
@@ -0,0 +1,6 @@
+from odoo import models, fields,api
+from odoo.exceptions import ValidationError
+from datetime import datetime
+
+
+    
