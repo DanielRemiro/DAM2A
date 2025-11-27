2a1b7ccd5cdd540bed976a159bc159ef619e2ae4
Daniel Remiro <remirolaguna23@iespabloserrano.com>
2025-11-27 18:06:12 +0100

    upd

--- SGE/odoo-compose/addons/openacademy/__manifest__.py
+++ SGE/odoo-compose/addons/openacademy/__manifest__.py
@@ -25,11 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
+        'views.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
-    ],
-}
+   
